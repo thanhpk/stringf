f82847eb2f869337cf02e7496542b7681edc76c7
Pham Kieu Thanh <thanhpk@live.com>
2017-06-06 11:33:32 +0700

    add js test

---
+++ test/stringf_test.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var stringf = require('../index.js');
+
+describe('stringf', function() {
+	it('should not format escape characters', function() {
+		var str = stringf.Format('hi ##name#name', { name: 'Kieu Thanh'});
+		assert.equal(str, 'hi #nameKieu Thanh');
+	});
+
+	it('should format parameters', function() {
+		var str = stringf.Format("hi #name ", { name: "thanh" });
+		assert.equal(str, 'hi thanh ');
+	});
+});
