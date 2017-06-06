bca4f61bbe3a67c506633eb0b53d08be3303eed9
Pham Kieu Thanh <thanhpk@live.com>
2017-06-06 11:34:51 +0700

    add golang implementation

---
+++ stringf.go
@@ -0,0 +1,44 @@
+package stringf
+
+// Format format s using map m
+// Keys of map m can be anything but must not contains spaces
+func Format(s string, m map[string]string) string {
+	ESCCHAR := byte('#');
+	i := 0
+	output := ""
+	for i < len(s) {
+		if s[i] == ESCCHAR {
+			j := i + 1
+			for j < len(s) && s[j] == ESCCHAR {
+				j++
+				if (j - i) % 2 == 0 {
+					output = output + string(ESCCHAR)
+				}
+			}
+			if (j - i) % 2 == 0 {
+				output = output + string(s[j])
+			} else {
+				param := ""
+				for j < len(s) && s[j] != byte(' ') {
+					param = param + string(s[j])
+					j++
+				}
+
+				if len(param) > 0 {
+					output = output + m[param]
+				}
+
+				if j == len(s) { return output }
+
+				if s[j] == byte(' ') {
+					output = output + " "
+				}
+			}
+			i = j + 1
+		} else {
+			output = output + string(s[i])
+			i++
+		}
+	}
+	return output
+}
