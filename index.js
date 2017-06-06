575ba9fa0c3f79501e9204eeaa4b74621de133d4
Pham Kieu Thanh <thanhpk@live.com>
2017-06-06 11:33:49 +0700

    add js implementation

---
+++ index.js
@@ -0,0 +1,41 @@
+exports.Format = function(s, m) {
+	var ESCCHAR = '#';
+	var i = 0;
+	var output = "";
+
+	while (i < s.length) {
+		if (s[i] == ESCCHAR) {
+			var j = i + 1;
+			while (j <= s.length && s[j] == ESCCHAR) {
+				j = j + 1;
+				if ((j - i) % 2 == 0 ) {
+					output = output + ESCCHAR;
+				}
+			}
+			if ((j - i) %2 == 0) {
+				output = output + s[j];
+			} else {
+				var param = "";
+				while (j < s.length && s[j] != ' ') {
+					param = param + s[j];
+					j++;
+				}
+				if (param.length > 0) {
+					output = output + m[param];
+				}
+				if (j == s.length) {
+					return output;
+				}
+
+				if (s[j] == ' ') {
+					output = output + ' ';
+				}
+			}
+			i = j + 1;
+		} else {
+			output = output + s[i];
+			i++;
+		}
+	}
+	return output;
+}
